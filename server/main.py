f185c0f3b027317a7acfa4c3254ff92725c83ba0
Shahil Patel <83348636+ShahilPatel-IITDh@users.noreply.github.com>
2026-05-10 09:41:20 +0530

    Added main function to run as a python file. Required for debugging purpose.

--- server/main.py
+++ server/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI # type: ignore
 from db.database import engine, Base
 from routes import auth_routes, node_routes
 
@@ -11,12 +11,17 @@ app = FastAPI(title="Sentinel Monitoring System")
 app.include_router(auth_routes.router)
 app.include_router(node_routes.router)
 
-
+# Basic endpoints for testing
 @app.get("/")
 def root():
     return {"message": "Sentinel Server Running"}
 
-
+# Health check endpoint
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    
